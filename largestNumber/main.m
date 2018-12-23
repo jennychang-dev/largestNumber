873bf4a4effb1ce70ac9835c99150cd5f141b430
Jenny Chang <chang.jc95@gmail.com>
2018-12-23 12:49:19 -0800

    yo

--- largestNumber/main.m
+++ largestNumber/main.m
@@ -10,8 +10,18 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *arrayOfNumbers = @[@12,@17,@25,@35,@81,@102];
+      
+        NSNumber *largestNumber = @0;
+        for (int i = 0; i < [arrayOfNumbers count]; i++) {
+            NSNumber *number = [arrayOfNumbers objectAtIndex:i];
+            
+            if ([number isGreaterThan:largestNumber]) {
+                largestNumber = number;
+                
+            }
+        }
+        NSLog(@"the largest number is %@",largestNumber);
     }
     return 0;
 }
